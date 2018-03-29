4ddfa1cdfe243f69bc823a339dcd0f10b4561c44
Tom <thomas.ulman@gmail.com>
2018-03-29 15:50:30 +1100

    added title and create board style with styled-components

--- src/App.js
+++ src/App.js
@@ -4,34 +4,52 @@ import styled from 'styled-components';
 
 import './App.css';
 
+
 const Title = styled.h1`
   font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+  text-align: left;
+  color: #2B7DE6FF;
+  padding: 1rem;
+  margin-left:4.5rem;
 `;
+
+const Board = styled.div`
+  display:flex;
+  width: 300px;
+  flex-wrap: wrap;
+`;
+
+const Square = styled.div`
+  width:100px;
+  height: 100px;
+  border:1px solid black;
+`;
+
+
 
 class App extends Component {
   render() {
     return (
         <div id="game">
 
-                <Title>
-      Hello World, this is my first styled component!
-    </Title>
+            <Title>
+              Tic Tac Toe
+            </Title>
+            
             <div id="head">
             </div>
   
-            <div id="board">
-              <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-                <div class="square"></div>
-            </div>
+            <Board>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+                <Square></Square>
+            </Board>
         </div>
     );
   }
